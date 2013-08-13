9c5e3bae9776539ccb72acdaf35b5ccf4211fd7b
John Iodice <iodice@pennmedicine.upenn.edu>
2013-08-13 17:42:42 +0000

    factor "apidb" schema out of tuning manager
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/TuningManager/trunk@12662 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- lib/sql/createTuningManager.sql
+++ lib/sql/createTuningManager.sql
@@ -1,4 +1,16 @@
-create table apidb.TuningTable (
+--
+-- createTuningManager.sql
+--
+-- create the persistent tables used for housekeeping by the tuning manager
+--
+-- usage:
+-- sqlplus <user>/<password>@<instance> [ schema. ]
+--
+-- the optional argument specifies the name of the schema in which the tables
+-- should be created. (The default is the login username; "user" and "schema"
+-- are somewhat interchangeable in Oracle.)
+
+create table &1 TuningTable (
    name          varchar2(65) primary key,
    timestamp     date not null,
    definition    clob not null,
@@ -7,31 +19,31 @@ create table apidb.TuningTable (
    check_os_user varchar2(20)
   );
 
-grant select on apidb.TuningTable to gus_r;
-grant insert, update, delete on apidb.TuningTable to gus_w;
+grant select on &1 TuningTable to gus_r;
+grant insert, update, delete on &1 TuningTable to gus_w;
 
-create table apidb.ObsoleteTuningTable (
+create table &1 ObsoleteTuningTable (
    name      varchar2(65) primary key,
    timestamp date not null);
 
-grant select on apidb.ObsoleteTuningTable to gus_r;
-grant insert, update, delete on apidb.ObsoleteTuningTable to gus_w;
+grant select on &1 ObsoleteTuningTable to gus_r;
+grant insert, update, delete on &1 ObsoleteTuningTable to gus_w;
 
-create sequence apidb.TuningManager_sq
+create sequence &1 TuningManager_sq
    start with 1111;
 
-grant select on apidb.TuningManager_sq to gus_w;
+grant select on &1 TuningManager_sq to gus_w;
 
-create table apidb.TuningMgrExternalDependency (
+create table &1 TuningMgrExternalDependency (
    name         varchar2(65) primary key,
    max_mod_date date,
    timestamp    date not null,
    row_count    number not null);
 
-grant select on apidb.TuningMgrExternalDependency to gus_r;
-grant insert, update, delete on apidb.TuningMgrExternalDependency to gus_w;
+grant select on &1 TuningMgrExternalDependency to gus_r;
+grant insert, update, delete on &1 TuningMgrExternalDependency to gus_w;
 
-create table apidb.InstanceMetaInfo as
+create table &1 InstanceMetaInfo as
 select sys_context ('USERENV', 'SERVICE_NAME') as instance_nickname,
        cast(null as varchar2(50)) as current_updater,
        cast(null as date) as update_start,
@@ -39,7 +51,7 @@ select sys_context ('USERENV', 'SERVICE_NAME') as instance_nickname,
        cast(null as varchar2(12)) as version
 from dual;
 
-grant select on apidb.InstanceMetaInfo to gus_r;
-grant insert, update, delete on apidb.InstanceMetaInfo to gus_w;
+grant select on &1 InstanceMetaInfo to gus_r;
+grant insert, update, delete on &1 InstanceMetaInfo to gus_w;
 
 exit
